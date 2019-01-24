60d59a1ad3108047f57fb488619307f2f387a239
yusra <yusrairfan99@hotmail.com>
2019-01-24 00:05:50 -0500

    added the box rotation!!

---
+++ yirfan3_Assignment1/Assets/__Scripts/BoxRightClickBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRightClickBehaviourScript : MonoBehaviour
+{
+  
+     public GameObject box;
+   
+   float z;
+   float y;
+   float x;
+
+
+ // Start is called before the first frame update
+      void Start()
+    {
+      x=box.transform.rotation.z;
+      z=box.transform.rotation.z;
+      y=box.transform.rotation.y;
+    }
+
+  
+    
+    // Update is called once per frame
+     public void Update()
+     {      
+        
+     }
+     void OnMouseOver () { //hover on an object
+    if(Input.GetMouseButtonDown(1)){//right click 
+        z+=5;
+        y=-5;
+        box.transform.rotation = Quaternion.Euler(new Vector3(x, y, z));
+    }
+     }
+}
---
+++ yirfan3_Assignment1/Assets/__Scripts/CameraSwitchBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitchBehaviourScript : MonoBehaviour
+{
+    public Camera sideCamera; //Side Camera
+    public Camera topCamera; //Top Camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sideCamera = Camera.main; //Gets the Main Camera from the Scene and assigns it to the side camera
+        sideCamera.enabled = true; //Enables Side Camera
+        topCamera.enabled = false; //Disable Top Camera
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)) //On pressing 'C' on the keyboard
+        {
+            //To switch cameras
+            if (sideCamera.enabled) 
+            {
+                topCamera.enabled = true; //Enables Top Camera
+                sideCamera.enabled = false; //Disable Side Camera
+            }
+            else
+            {
+                topCamera.enabled = false; //Disable Top Camera
+                sideCamera.enabled = true; //Enables Side Camera
+            }
+        }
+    }
+}
