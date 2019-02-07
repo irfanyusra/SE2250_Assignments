a4909867e6ac428900de338051f49a9d430bb269
yusra <yusrairfan99@hotmail.com>
2019-02-06 22:56:59 -0500

    basics of the game done

---
+++ yirfan3_Assignment2/Assets/__Scripts/PickupObjectSpawnerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObjectSpawnerScript : MonoBehaviour
+{
+    public GameObject pickupObjectPrefabVar;
+    public List <GameObject> gameObjectList;
+     
+     public int numOfObjectsOnTheScene;
+     int numPickupObjects = 0;
+     float radius = 9f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjectList=new List<GameObject>();
+      while (numPickupObjects<= numOfObjectsOnTheScene){
+          int objectScore =  Random.Range(1, 4);
+        if(objectScore == 1)
+            addObjects (objectScore, Color.green);  
+        else if (objectScore == 2)
+            addObjects (objectScore, Color.red); 
+        else if (objectScore==3) 
+            addObjects (objectScore, Color.yellow); 
+        numPickupObjects++;
+       }
+    }
+
+    
+    void addObjects (int objectScore, Color color){
+
+        GameObject gameObject = Instantiate <GameObject> (pickupObjectPrefabVar);
+        gameObject.name = "PickupObj"+ numPickupObjects +" - " + objectScore;
+        gameObject.GetComponent<Renderer>().material.color = color;
+        float angle = numPickupObjects * Mathf.PI*2f / numOfObjectsOnTheScene;
+        Vector3 newPos = new Vector3(Mathf.Cos(angle)*radius, 1, Mathf.Sin(angle)*radius);
+        gameObject.tag="PickUp"+objectScore.ToString();
+        gameObject.transform.position = newPos;//new Vector3 (xpos,1.5f,zpos);//Random.insideUnitSphere;
+        gameObjectList.Add(gameObject);
+
+    }
+
+
+    
+}
---
+++ yirfan3_Assignment2/Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+ public float speed;
+ public Text scoreText;
+ private Rigidbody rigidbody;
+
+
+int score;
+// Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent <Rigidbody>();
+        score =0;
+        SetScoreText();
+     
+    }
+
+  
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+        rigidbody.AddForce (movement*speed); 
+    }
+
+ void OnTriggerEnter(Collider other){
+     if (other.gameObject.CompareTag("PickUp1")){
+         other.gameObject.SetActive(false);
+         score+=1;
+     }
+      if (other.gameObject.CompareTag("PickUp2")){
+         other.gameObject.SetActive(false);
+         score+=2;
+     }
+      if (other.gameObject.CompareTag("PickUp3")){
+         other.gameObject.SetActive(false);
+         score+=3;
+     }
+      scoreText.text= "Score: " + score ;  
+
+ }
+    
+    void SetScoreText(){
+        scoreText.text= "Score: " + score ;  
+     }
+}
